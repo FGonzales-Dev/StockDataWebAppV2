edf06ddca464fb0f0537737ead5da4092a4b7147
francis gonzales <fgonzales.private@icloud.com>
2022-09-26 21:36:44 +0800

    Update settings.py

--- cb_dj_weather_app/settings.py
+++ cb_dj_weather_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bkr(1mx&+l80n275l=j6rz!4@qzfb$^-n86xqc*+sj9b*kz9gm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0','ancient-dawn-83050.herokuapp.com','127.0.0.1']
 
