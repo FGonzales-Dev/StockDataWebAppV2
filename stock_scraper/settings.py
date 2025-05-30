6373d980e9cab34355b379d906514c652408aa24
Francis Gonzales <francisgonzales.private@gmail.com>
2025-05-31 06:40:10 +0800

    Fix deprecated timezone: change Asia/Chungking to Asia/Shanghai

--- stock_scraper/settings.py
+++ stock_scraper/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Chungking'
+TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = False
